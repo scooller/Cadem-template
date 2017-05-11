92988565086cd9f2451c9acf4e956b929e236b7c
scooller <scoollerx@hotmail.com>
2017-05-11 15:40:46 -0300

    Slider

--- front-page.php
+++ front-page.php
@@ -1,42 +1,5 @@
 ﻿<?php get_header(); ?>
-<div id="carousel-home" class="carousel slide" data-ride="carousel">
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">      
-      
-		<div class="row">
-		<div class="col-sm-4">
-			<div class="box verde bg fix" style="background-image: url(<?php bloginfo('template_url'); ?>/img/dummy/cademcl-home_03.png)">
-				<h1 class="big sub-title v-align"><strong>42 años</strong> en el mercado<br>
-				120 profesionales<br>
-				Panel de <strong>4000</strong> participantes</h1>
-			</div>
-		</div>
-		<div class="col-sm-4">
-			<div class="box-mini azul bg" style="background-image: url(<?php bloginfo('template_url'); ?>/img/dummy/cademcl-home_04.png);"><h4 class="v-align title">NUEVA ENCUESTA PLAZA PÚBLICA</h4></div>
-			<div class="box-mini fucia">
-				<h4>Las preferencias laborales de los millennials.</h4>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-			</div>
-		</div>
-		<div class="col-sm-4">
-			<div class="box-mini verde bg" style="background-image: url(<?php bloginfo('template_url'); ?>/img/dummy/cademcl-home_05.png)"><h4 class="v-align title">MUJERES EN LAS EMPRESAS</h4></div>
-			<div class="box-mini azul bg" style="background-image: url(<?php bloginfo('template_url'); ?>/img/dummy/cademcl-home_07.png);"><h4 class="v-align title">MARCAS CIUDADANAS</h4></div>
-		</div>
-		</div>
-    </div>
-  </div>
-
-  <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-home" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#carousel-home" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+<?php get_template_part( 'slider' ); ?>
 <section class="row mini">
 	<div class="col-md-3 azul">
 		<h2 class="v-align title light">Nuestros Productos</h2>
---
+++ slider.php
@@ -0,0 +1,57 @@
+<div id="carousel-home" class="carousel slide" data-ride="carousel">
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">      
+      
+		<div class="row">
+		<div class="col-sm-4">
+			<div class="box azul bg fix" style="background-image: url(<?php bloginfo('template_url'); ?>/img/dummy/cademcl-home_03.png)">
+				<h1 class="big sub-title v-align"><strong>42 años</strong> en el mercado<br>
+				120 profesionales<br>
+				Panel de <strong>4000</strong> participantes</h1>
+			</div>
+		</div>
+		<div class="col-sm-4">
+			<div class="box-mini azul bg" style="background-image: url(<?php bloginfo('template_url'); ?>/img/dummy/cademcl-home_04.png);"><h4 class="v-align title">NUEVA ENCUESTA PLAZA PÚBLICA</h4></div>
+			<div class="box-mini fucsia">
+				<h4>Las preferencias laborales de los millennials.</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			</div>
+		</div>
+		<div class="col-sm-4">
+			<div class="box-mini fucsia bg" style="background-image: url(<?php bloginfo('template_url'); ?>/img/dummy/cademcl-home_05.png)"><h4 class="v-align title">MUJERES EN LAS EMPRESAS</h4></div>
+			<div class="box-mini fucsia bg" style="background-image: url(<?php bloginfo('template_url'); ?>/img/dummy/cademcl-home_07.png);"><h4 class="v-align title">MARCAS CIUDADANAS</h4></div>
+		</div>
+		</div>
+    </div>
+    <!-- Item -->
+    <div class="item">      
+      
+		<div class="row">
+		<div class="col-sm-3">
+			<div class="box-mini verde"><h4 class="v-align title">DESAPROBACIÓN DE BACHELET</h4></div>
+			<div class="box-mini fucsia"><h4 class="v-align title">Lorem ipsum dolor sit amet, consectetur </h4></div>
+		</div>
+		<div class="col-sm-3">
+			<div class="box-mini verde"><h4 class="v-align title">Lorem ipsum dolor sit amet, consectetur </h4></div>
+			<div class="box-mini azul"><h4 class="v-align title">Lorem ipsum dolor sit amet, consectetur </h4></div>
+		</div>
+		<div class="col-sm-6">
+			<div class="box-mini azul"><h4 class="v-align title">QUINTO ESTUDIO SOBRE CONFIANZA EMPRESARIAL</h4></div>
+			<div class="box-mini verde"><h4 class="v-align title">Lorem ipsum dolor sit amet</h4></div>
+		</div>
+		</div>
+   
+    </div>
+  </div>
+
+  <!-- Controls -->
+  <!--<a class="left carousel-control" href="#carousel-home" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>-->
+  <a class="right carousel-control" href="#carousel-home" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
